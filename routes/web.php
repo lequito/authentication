6ee65939d05dd3734b1ca71a32738439522f002c
Lekito <alexdossantosdias1212@gmail.com>
2025-02-26 18:07:34 -0300

    creation of Authcontroller and login form

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('guest')->group(function(){
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'authenticate'])->name('authenticate');
 });
+
+Route::middleware('auth')->group(function(){
+    Route::get('/', function(){
+        echo 'Olá mundo';
+    });
+});
+
+
