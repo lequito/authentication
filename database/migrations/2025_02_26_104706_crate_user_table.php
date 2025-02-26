f85c71d7268ac8420238ebfbc1c89e1fb2464671
Lekito <alexdossantosdias1212@gmail.com>
2025-02-26 07:58:33 -0300

    creation of user migrations

---
+++ database/migrations/2025_02_26_104706_crate_user_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration{
+   
+    public function up(): void{
+        Schema::create('users', function(Blueprint $table){
+            $table->id();
+            $table->string('username', 30)->unique();
+            $table->string('email', 50)->unique();
+            $table->string('password', 200);
+            $table->string('token', 100)->nullable();
+            $table->dateTime('email_verified_at')->nullable()->default(null);
+            $table->dateTime('last_login_at')->nullable()->default(null);
+            $table->boolean('active')->nullable()->default(null);
+            $table->dateTime('blocked_until')->nullable()->default(null);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down(): void{
+        //
+    }
+};
